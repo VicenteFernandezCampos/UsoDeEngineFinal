eb38e8cf8ea678892aca0e1e5a4cf650b02685b7
VicenteFernandezCampos <80338998+VicenteFernandezCampos@users.noreply.github.com>
2021-11-18 11:55:09 -0300

    Canvas y TMPro

--- Encargo N°4 Templo de Físicas/Assets/Script/AudioManager.cs
+++ Encargo N°4 Templo de Físicas/Assets/Script/AudioManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text text;
+    public AudioMixer audioMixer;
+    public AudioMixerGroup BMGMixerGroup;
+    public AudioMixerGroup SFXMixerGroup;
+    public AudioSource BMGAudioSource;
+    public AudioSource SFXAudioSource;
+    public AudioClip audioClip0;
+    public AudioClip audioClip1;
+    public AudioClip audioClip2;
+    public AudioClip audioClip3;
+    public AudioClip audioClip4;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float value;
+
     }
 }
