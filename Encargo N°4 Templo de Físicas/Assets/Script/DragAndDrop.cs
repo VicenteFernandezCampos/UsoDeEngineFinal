1b403bbf6898f1189469a6da69edefccc16ed5f3
CyberneticSheep <80337466+CyberneticSheep@users.noreply.github.com>
2021-11-18 21:21:23 -0300

    Empujar y caja

---
+++ Encargo N°4 Templo de Físicas/Assets/Script/DragAndDrop.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject handPoint;
+
+    GameObject pickedObject = null;
+
+    void Update()
+    {
+        if (pickedObject != null)
+        {
+            pickedObject.GetComponent<Rigidbody>().useGravity = true;
+            pickedObject.GetComponent<Rigidbody>().isKinematic = false;
+            pickedObject.gameObject.transform.SetParent(null);
+            pickedObject = null;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Object"))
+        {
+            if(Input.GetKey(KeyCode.Mouse0) && pickedObject == null)
+            {
+                other.GetComponent<Rigidbody>().useGravity = false;
+                other.GetComponent<Rigidbody>().isKinematic = true;
+                other.transform.position = handPoint.transform.position;
+                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+                pickedObject = other.gameObject;
+            }
+        }
+    }
+}
