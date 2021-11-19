018573a4d692133539a2dd7c5d1acf671c2cd872
CyberneticSheep <80337466+CyberneticSheep@users.noreply.github.com>
2021-11-18 21:54:54 -0300

    Nivel empujar listo

---
+++ Encargo N°4 Templo de Físicas/Assets/Script/OpenTriggerDoor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTriggerDoor : MonoBehaviour
+{
+    public Animator myDoor = null;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myDoor.Play("DoorOpen", 0, 0.0f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myDoor.Play("DoorClose", 0, 0.0f);
+        }
+    }
+}
