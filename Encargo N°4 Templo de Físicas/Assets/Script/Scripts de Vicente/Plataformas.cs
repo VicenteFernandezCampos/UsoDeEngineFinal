1422e9a31ba65d141985ee4f286e4b9204cb1fcf
VicenteFernandezCampos <80338998+VicenteFernandezCampos@users.noreply.github.com>
2021-11-18 22:34:17 -0300

    Menu, Dolly y Puzzle 2

--- Encargo N°4 Templo de Físicas/Assets/Script/Scripts de Vicente/Plataformas.cs
+++ Encargo N°4 Templo de Físicas/Assets/Script/Scripts de Vicente/Plataformas.cs
@@ -1,10 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Plataformas : MonoBehaviour
 {
-    public GameObject point1, point2;
+    public Transform theTransform, refIzq, refDer;
+    public Transform target;
+    public float speed = 3;
+    private void Start()
+    {
+        target = refDer;
+    }
+    private void Update()
+    {
+        if (Vector3.Distance(theTransform.position, target.position) > 0.1f)
+        {
+            theTransform.position = Vector3.MoveTowards(theTransform.position, target.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            if (target == refDer)
+            {
+                target = refIzq;
+            }
+            else if (target == refIzq)
+            {
+                target = refDer;
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
